5f7f6f6f90a1f3019c960a6c4db26b889927e3ab
kksofii <126670569+kksofii@users.noreply.github.com>
2024-10-02 10:37:29 -0300

    Add files via upload

---
+++ analise-de-dados-aula09/graficos/RedesFavoritasMundo.js
@@ -0,0 +1,24 @@
+import { getCSS, criarGrafico, incluirTexto } from "./common.js"
+
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
+
+    const data = [
+        {
+            values: valores,
+            labels: redes,
+            type: 'pie',
+            textinfo: 'label+percent'
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        height: 700,
+        title: {
+            
